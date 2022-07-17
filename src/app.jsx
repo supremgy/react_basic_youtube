f6f033f6f887699e630eeecb1c6cdae03cc91628
James <supremgy@gmail.com>
2022-07-17 23:44:52 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,25 @@
-
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_llst";
 
 function App() {
-  return <h1>Seedra said Ya</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyCtez6NC88J0WBK5Bv3wI3V9LZb0aMIYiY",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
